da6a635f8215af8b5abc46f7727b11fc2a4dc2a0
Ninh Doan <uitnetwork@gmail.com>
2018-04-04 20:56:51 +0800

    Move testProjects

--- test-plugin/src/test/groovy/com/uitnetwork/TestPluginTest.groovy
+++ test-plugin/src/test/groovy/com/uitnetwork/TestPluginTest.groovy
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat
 import static org.gradle.testkit.runner.TaskOutcome.UP_TO_DATE
 
 class TestPluginTest extends GroovyTestCase {
-    def projectDir = new File("/tmp/gradle-plugin/test-plugin/src/test/resources/testProjects")
+    def projectDir = new File(System.getProperty("user.dir") + "/testProjects")
     def pluginClasspathResource = getClass().classLoader.getResource("plugin-classpath.txt")
     def pluginClasspath = pluginClasspathResource.readLines().collect { new File(it) }
 
